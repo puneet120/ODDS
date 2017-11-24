bd9797e8c8599a4de224db2e4f7c4e63e0553269
puneet120 <33218769+puneet120@users.noreply.github.com>
2017-11-24 20:50:15 +0530

    Changes in RabbitMQ File

--- OddsMessaging/OddsMessaging/Controllers/MessagingController.cs
+++ OddsMessaging/OddsMessaging/Controllers/MessagingController.cs
@@ -18,6 +18,9 @@ namespace OddsMessaging.Controllers
 
     public class MessagingController : ApiController
     {
+        private static IConnection connection;
+        private static IModel channel;
+       
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -68,9 +71,7 @@ namespace OddsMessaging.Controllers
                                         routingKey: "oddsqueue",
                                         basicProperties: null,
                                         body: body);
-
-
-                OddsHub.BroadcastData();
+             
                 return Ok(model);
             }
 
@@ -80,55 +81,39 @@ namespace OddsMessaging.Controllers
         [Route("odd/get")]
         public HttpResponseMessage GetAllOdds()
         {
-            var factory = new ConnectionFactory()
+            OddsModel data = new OddsModel();     
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "oddsqueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+           
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                HostName = "localhost"
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                var deserialized = JsonConvert.DeserializeObject<OddsModel>(message);
 
-            };
-
-            var data = new OddsModel();
-
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "oddsqueue",
-                                        durable: false,
-                                        exclusive: false,
-                                        autoDelete: false,
-                                        arguments: null);
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                data = new OddsModel
                 {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    var deserialized = JsonConvert.DeserializeObject<OddsModel>(message);
-
-                    data= new OddsModel
-                    {
-                        Description = deserialized.Description,
-                        Odd_1= deserialized.Odd_1,
-                        Odd_X= deserialized.Odd_X,
-                        Odd_2= deserialized.Odd_2,
-                        Id= deserialized.Id
-                      
-                    };
-
+                    Description = deserialized.Description,
+                    Odd_1 = deserialized.Odd_1,
+                    Odd_X = deserialized.Odd_X,
+                    Odd_2 = deserialized.Odd_2,
+                    Id = deserialized.Id
 
                 };
-                channel.BasicConsume(queue: "oddsqueue",
-                                        autoAck: true,
-                                        consumer: consumer);
 
+                OddsHub.BroadcastData(data);
 
-
-                Thread.Sleep(6000);
-
-                return Request.CreateResponse(HttpStatusCode.OK, data);
-
-
-            }
+                // Display message
+            };
+            channel.BasicConsume(queue: "oddsqueue", autoAck: true, consumer: consumer); 
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        
         }
-      
+     
+
     }
 }
 
--- OddsMessaging/OddsMessaging/Hubs/OddsHub.cs
+++ OddsMessaging/OddsMessaging/Hubs/OddsHub.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using OddsMessaging.Models;
 
 namespace OddsMessaging.Hubs
 {
     public class OddsHub : Hub
     {
         [HubMethodName("broadcastData")]
-        public static void BroadcastData()
+        public static void BroadcastData(OddsModel model)
         {
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<OddsHub>();
-            context.Clients.All.updatedData();
+            context.Clients.All.updatedData(model);
         }
     }
 }
