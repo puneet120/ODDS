efde26680057916832e01a56cee3830b1d0411cb
puneet120 <33218769+puneet120@users.noreply.github.com>
2017-12-01 09:49:01 +0530

    Services.Data.Repository

---
+++ OddsBusiness/Odds.Services.Data.Repository/Dependency/DataModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using OddsBusiness.Repository.Interfaces;
+using OddsBusiness.ORM.Context;
+using OddsBusiness.Repository.Implementation;
+
+namespace OddsBusiness.Repository.Dependency
+{
+    public class DataModule : Module
+    {
+        private string connStr;
+        public DataModule(string connString)
+        {
+            this.connStr = connString;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new EFContext(this.connStr)).As<IDbContext>().InstancePerRequest();
+            builder.RegisterGeneric(typeof(SqlRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();           
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerRequest();         
+            builder.RegisterType<Logger>().As<ILogger>().InstancePerRequest();
+            builder.RegisterType<OddRepository>().As<IOddRepository>().InstancePerRequest();
+            base.Load(builder);
+        }
+    }
+}
