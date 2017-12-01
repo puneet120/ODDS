e4916501d5cd0fbc8fc0c64ec311c229c5ace4f6
puneet120 <33218769+puneet120@users.noreply.github.com>
2017-12-01 09:59:43 +0530

    Odds.Services internal files

---
+++ OddsBusiness/Odds.Services/Models/OddsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OddsBusiness.Models
+{
+    public class OddsModel:BaseModel
+    {
+
+        public string Description { get; set; }
+
+        public string Odd_1 { get; set; }
+
+        public string  Odd_X { get; set; }
+
+        public string Odd_2 { get; set; }
+
+    }
+}
---
+++ OddsBusiness/Odds.Services/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OddsBusiness.Startup))]
+namespace OddsBusiness
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
